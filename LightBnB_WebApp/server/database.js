713ee5dde78cda3c3bafb2feca6fa232edb702cb
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-26 19:17:54 +0000

    Implement PSQL queries to User Sing Up/Log In

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -20,16 +20,22 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  // let user;
+  // for (const userId in users) {
+  //   user = users[userId];
+  //   if (user.email.toLowerCase() === email.toLowerCase()) {
+  //     break;
+  //   } else {
+  //     user = null;
+  //   }
+  // }
+  // return Promise.resolve(user);
+  const queryString = `
+  SELECT * FROM users
+  WHERE email = $1;
+  `;
+  return pool.query(queryString, [email])
+  .then(res => res.rows[0]);  
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -39,7 +45,13 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  // return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT * FROM users
+  WHERE id = $1;
+  `;
+  return pool.query(queryString, [id])
+  .then(res => res.rows[0]);  
 }
 exports.getUserWithId = getUserWithId;
 
@@ -50,10 +62,19 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
+  const values = [user.name, user.email, user.password]
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+  return pool.query(queryString, values)
+  .then(res => res.rows[0]); 
+
 }
 exports.addUser = addUser;
 
